082fab6170e39031eacdebdf6bd4b52902aeaaaa
Johan Fredin <johan.fredin@retroidinteractive.com>
2014-10-12 22:14:35 +0200

    Cleanup and further javadoc

--- src/main/java/se/fredin/gdxtensions/level/Level.java
+++ src/main/java/se/fredin/gdxtensions/level/Level.java
@@ -1,17 +1,25 @@
 package se.fredin.gdxtensions.level;
 
 import se.fredin.gdxtensions.screen.BaseScreen;
-import se.fredin.gdxtensions.utils.ScreenType;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
+/**
+ * Very general superclass for any kind of game level, contains a camera and a shaperenderer
+ * @author Johan Fredin
+ *
+ * @param <T> any class that extends the BaseScreen
+ */
 public abstract class Level<T extends BaseScreen> implements LevelBase {
 
 	protected OrthographicCamera camera;
 	protected ShapeRenderer shapeRenderer;
 	
+	/**
+	 * Instantiate the camera and shaperenderer
+	 * @param screen the screen where we can retrieve further objects and switch levels with.
+	 */
 	public Level(T screen) {
 		this.camera = screen.getCamera();
 		this.shapeRenderer = new ShapeRenderer();
@@ -19,38 +27,7 @@ public abstract class Level<T extends BaseScreen> implements LevelBase {
 	
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void switchLevel() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void restart(boolean allowedToShowAdd) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void end(ScreenType screenToGoTo, boolean allowedToShowAdd) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void tick(float deltatime) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void render(SpriteBatch batch, OrthographicCamera camera) {
-		// TODO Auto-generated method stub
-
+		shapeRenderer.dispose();
 	}
 
 }
--- src/main/java/se/fredin/gdxtensions/level/TiledMapLevel.java
+++ src/main/java/se/fredin/gdxtensions/level/TiledMapLevel.java
@@ -3,31 +3,29 @@ package se.fredin.gdxtensions.level;
 import se.fredin.gdxtensions.res.Assets;
 import se.fredin.gdxtensions.screen.BaseScreen;
 
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
+/***
+ * Extension of Level, used when we want to create a level that works with a tiled map
+ * created in Tiled or any other editor supporting the .TMX format. contains a TiledMap and a
+ * OrthogonalTiledMapRenderer for further functionality
+ * @author johan
+ *
+ * @param <T>
+ */
 @SuppressWarnings("rawtypes")
 public abstract class TiledMapLevel<T extends BaseScreen> extends Level implements LevelBase {
 	
 	protected OrthogonalTiledMapRenderer mapRenderer;
 	protected TiledMap map;
 	
+	
 	@SuppressWarnings("unchecked")
 	public TiledMapLevel(String levelName, T screen) {
 		super(screen);
 		this.map = (TiledMap) Assets.getInstance().get(levelName);
 		this.mapRenderer = new OrthogonalTiledMapRenderer(map, screen.getSpriteBatch());
-	}
-	
-	public OrthographicCamera getCamera() {
-		return camera;
-	}
-
-	@SuppressWarnings("unchecked")
-	public void setCamera(OrthographicCamera camera) {
-		this.camera = camera;
 	}
 	
 	public void setMapRenderer(OrthogonalTiledMapRenderer mapRenderer) {
@@ -46,17 +44,9 @@ public abstract class TiledMapLevel<T extends BaseScreen> extends Level implemen
 		this.map = map;
 	}
 	
-	@SuppressWarnings("unchecked")
-	public void setShapeRenderer(ShapeRenderer shapeRenderer) {
-		this.shapeRenderer = shapeRenderer;
-	}
-	
-	public ShapeRenderer getShapeRenderer() {
-		return shapeRenderer;
-	}
-
 	@Override
 	public void dispose() {
+		super.dispose();
 		mapRenderer.dispose();
 	}
 
