0ec06d47b049401a4bff4bd7c51aa4497e6f170d
Johan Fredin <johanfredin204@hotmail.com>
2014-12-23 22:45:16 +0100

    Updated new dialog classes with javadoc

--- src/main/java/se/fredin/gdxtensions/scene2d/dialog/Dialog.java
+++ src/main/java/se/fredin/gdxtensions/scene2d/dialog/Dialog.java
@@ -1,6 +1,7 @@
 package se.fredin.gdxtensions.scene2d.dialog;
 
 import se.fredin.gdxtensions.font.AnimatedBitmapFont;
+import se.fredin.gdxtensions.scene2d.dialog.DialogOpenAndCloseOptions.OpenCloseAnimation;
 import se.fredin.gdxtensions.utils.text.AnimatedText;
 import se.fredin.gdxtensions.utils.text.OutputFormatter;
 import se.fredin.gdxtensions.utils.text.OutputFormatter.LineBreakSettings;
@@ -12,18 +13,25 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextArea;
 
 /**
  * A class to represent a dialog box with a text in it. 
- * Think Zelda or any RPG game and you'll get the point
+ * Think Zelda or any RPG game and you'll get the point.
+ * The dialog class is actually a {@link TextArea} with an extra {@link Image} used as a frame.
  * @author Johan Fredin
  *
  */
 public class Dialog extends TextArea {
 	
-	public static final int TOP = 0, LEFT = 1, BOTTOM = 2, RIGHT = 3;
+	/** The default padding to use for the dialog e.g the frame */
+	public static float defaultPadding = 5f;
+	/** The default width for each side of the frame */
+	public static float[] defaultPaddingSettings = {defaultPadding, defaultPadding, defaultPadding, defaultPadding};
+	/** The default duration of the frame animation */
 	public static float defaultDuration = .66f;
 	
-	public static float defaultPadding = 5;
-	public static float[] defaultPaddingSettings = {defaultPadding, defaultPadding, defaultPadding, defaultPadding};
-	
+	private final byte TOP = 0;
+	private final byte LEFT = 1; 
+	private final byte BOTTOM = 2; 
+	private final byte RIGHT = 3;
+
 	private float dialogWidth;
 	private float dialogHeight;
 	private float frameWidth;
@@ -36,18 +44,57 @@ public class Dialog extends TextArea {
 	private AnimatedText animatedText;
 	private LineBreakSettings lineBreakSettings;
 	
+	/**
+	 * Create a new dialog with default settings for text animation speed, linebreak index, linebreak settings and padding.
+	 * @param text the text to use for this dialog box
+	 * @param style the {@link TextFieldStyle}
+	 */
 	public Dialog(String text, TextFieldStyle style) {
-		this(text, style, .005f, OutputFormatter.DEFAULT_LINEBREAK_INDEX, LineBreakSettings.NEXT_SEQUENCE, defaultPaddingSettings);
+		this(text, style, AnimatedText.DEFAULT_TIME_PER_CHARACTER, OutputFormatter.DEFAULT_LINEBREAK_INDEX, LineBreakSettings.NEXT_SEQUENCE, defaultPaddingSettings);
 	}
 	
+	/**
+	 * Create a new dialog with custom settings for animation speed and default settings for linebreak index, linebreak settings and padding.
+	 * @param text the text to use for this dialog box
+	 * @param style the {@link TextFieldStyle}
+	 * @param timePerCharacter the update speed of the text to be displayed in the box.
+	 */
 	public Dialog(String text, TextFieldStyle style, float timePerCharacter) {
 		this(text, style, timePerCharacter, OutputFormatter.DEFAULT_LINEBREAK_INDEX, OutputFormatter.DEFAULT_LINEBREAK_SETTINGS, defaultPaddingSettings);
 	}
 	
+	/**
+	 * Create a new dialog with custom settings for animation speed, linebreak index and default settings for linebreak settings and padding.
+	 * @param text the text to use for this dialog box
+	 * @param style the {@link TextFieldStyle}
+	 * @param timePerCharacter the update speed of the text to be displayed in the box.
+	 * @param lineBreakIndex the index in the text where we want a line break.
+	 */
 	public Dialog(String text, TextFieldStyle style, float timePerCharacter, short lineBreakIndex) {
 		this(text, style, timePerCharacter, lineBreakIndex, OutputFormatter.DEFAULT_LINEBREAK_SETTINGS, defaultPaddingSettings);
 	}
 	
+	/**
+	 * Create a new dialog with custom settings for animation speed, linebreak, linebreak settings and  default settings for padding.
+	 * @param text the text to use for this dialog box
+	 * @param style the {@link TextFieldStyle}
+	 * @param timePerCharacter the update speed of the text to be displayed in the box.
+	 * @param lineBreakIndex the index in the text where we want a line break.
+	 * @param the {@link LineBreakSettings} to use
+	 */
+	public Dialog(String text, TextFieldStyle style, float timePerCharacter, short lineBreakIndex, LineBreakSettings settings) {
+		this(text, style, timePerCharacter, lineBreakIndex, settings, defaultPaddingSettings);
+	}
+	
+	/**
+	 * Create a new dialog with custom settings for animation speed, linebreak index, linebreak settings and padding.
+	 * @param text the text to use for this dialog box
+	 * @param style the {@link TextFieldStyle}
+	 * @param timePerCharacter the update speed of the text to be displayed in the box.
+	 * @param lineBreakIndex the index in the text where we want a line break.
+	 * @param lineBreakSettings the {@link LineBreakSettings} to use
+	 * @param padding the padding to use for each side (top, left, bottom, right)
+	 */
 	public Dialog(String text, TextFieldStyle style, float timePerCharacter, short lineBreakIndex, LineBreakSettings lineBreakSettings, float[] padding) {
 		super("", style);
 		this.frame = new Image(style.background);
@@ -72,37 +119,64 @@ public class Dialog extends TextArea {
 		this.frame.setSize(0, 0);
 		this.frame.setColor(Color.BLUE);
 		
-		this.openDialog(.8f);
+		this.openDialog();
 	}
 	
+	/**
+	 * @return the animated text used in this dialog
+	 */
 	public AnimatedText getAnimatedText() {
 		return animatedText;
 	}
 	
+	/**
+	 * Set the animation speed for the text
+	 * @param timePerCharacter the time for each character until updating
+	 */
 	public void setTimePerCharacter(float timePerCharacter) {
 		this.timePerCharacter = timePerCharacter;
 	}
 	
+	/**
+	 * @return the animation speed for the text
+	 */
 	public float getTimePerCharacter() {
 		return timePerCharacter;
 	}
 	
+	/**
+	 * Set the linebreak index for the text
+	 * @param lineBreakIndex
+	 */
 	public void setLineBreakIndex(short lineBreakIndex) {
 		this.lineBreakIndex = lineBreakIndex;
 	}
 	
+	/**
+	 * @return the index in the text where there is a line break.
+	 */
 	public short getLineBreakIndex() {
 		return lineBreakIndex;
 	}
 	
+	/**
+	 * Set the {@link LineBreakSettings} for the text
+	 * @param lineBreakSettings
+	 */
 	public void setLineBreakSettings(LineBreakSettings lineBreakSettings) {
 		this.lineBreakSettings = lineBreakSettings;
 	}
 	
+	/**
+	 * @return the {@link LineBreakSettings} for the text
+	 */
 	public LineBreakSettings getLineBreakSettings() {
 		return lineBreakSettings;
 	}
 	
+	/**
+	 * @return the frame {@link Image}
+	 */
 	public Image getFrame() {
 		return frame;
 	}
@@ -129,35 +203,97 @@ public class Dialog extends TextArea {
 		}
 	}
 	
+	/**
+	 * @return whether or not its time to close this dialog
+	 */
 	public boolean isTimeToCloseDialog() {
 		return openCloseOptions.isTimeToCloseDialog();
 	}
 	
+	/**
+	 * @return the width of the frame surrounding the dialog
+	 */
 	public float getFrameWidth() {
 		return frameWidth;
 	}
 	
+	/**
+	 * @return the height of the frame surrounding the dialog
+	 */
 	public float getFrameHeight() {
 		return frameHeight;
 	}
 	
+	/**
+	 * Updates the {@link AnimatedText} and sets the text of this dialog char by char
+	 * @param delta
+	 */
 	public void tick(float delta) {
 		animatedText.tick(delta);
 	    setText(animatedText.getCurrentText());
 	}
 	
+	
+	/**
+	 * Makes the dialog open. Will use the default {@link OpenCloseAnimation#QUAD_EVEN}
+	 * and {@link Dialog#defaultDuration} if no values for these have been previously set.
+	 */
+	public void openDialog() {
+		openCloseOptions.openDialog();
+	}
+	
+	/**
+	 * Makes the dialog close. Will use the default {@link OpenCloseAnimation#QUAD_EVEN}
+	 * and {@link Dialog#defaultDuration} if no values for these have been previously set.
+	 */
+	public void closeDialog() {
+		openCloseOptions.closeDialog();
+	}
+	
+	/**
+	 * Makes the dialog open with custom duration.
+	 * will use {@link OpenCloseAnimation#QUAD_EVEN} if none has been previously set
+	 * @param duration the duration of the animation
+	 */
 	public void openDialog(float duration) {
-		this.openCloseOptions.openDialog(duration);
+		openCloseOptions.openDialog(duration);
 	}
 	
+	/**
+	 * Makes the dialog close with custom duration.
+	 * will use {@link OpenCloseAnimation#QUAD_EVEN} if none has been previously set
+	 * @param duration the duration of the animation
+	 */
 	public void closeDialog(float duration) {
-		this.openCloseOptions.closeDialog(duration);
+		openCloseOptions.closeDialog(duration);
 	}
 	
+	/**
+	 * Makes the dialog open with custom animation. will use {@link Dialog#defaultDuration} if none is previously set.
+	 * @param openCloseAnimation the animation to use
+	 */
+	public void openDialog(OpenCloseAnimation openCloseAnimation) {
+		openCloseOptions.openDialog(openCloseAnimation);
+	}
+	
+	/**
+	 * Makes the dialog close with custom animation. will use {@link Dialog#defaultDuration} if none is previously set.
+	 * @param openCloseAnimation the animation to use
+	 */
+	public void closeDialog(OpenCloseAnimation openCloseAnimation) {
+		openCloseOptions.closeDialog(openCloseAnimation);
+	}
+	
+	/***
+	 * @return the width of the dialog e.g the inner image
+	 */
 	public float getDialogWidth() {
 		return dialogWidth;
 	}
 	
+	/**
+	 * @return the height of the dialog e.g the inner frame
+	 */
 	public float getDialogHeight() {
 		return dialogHeight;
 	}
--- src/main/java/se/fredin/gdxtensions/scene2d/dialog/DialogOpenAndCloseOptions.java
+++ src/main/java/se/fredin/gdxtensions/scene2d/dialog/DialogOpenAndCloseOptions.java
@@ -1,17 +1,15 @@
 package se.fredin.gdxtensions.scene2d.dialog;
 
 import se.fredin.gdxtensions.utils.LogUtils;
-import se.fredin.gdxtensions.utils.MVPair;
 
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-import com.badlogic.gdx.scenes.scene2d.actions.AfterAction;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 /**
- * Utility class for handeling animation of dialog boxes as they open and close
+ * Utility class for handling animation of dialog boxes as they open and close
  * @author Johan Fredin
  *
  */
@@ -32,18 +30,38 @@ public class DialogOpenAndCloseOptions {
 	private boolean isTimeToCloseDialog;
 	private boolean canTick;
 	
+	/**
+	 * Construct a new dialog with no {@link Interpolation} and {@link Dialog#defaultDuration}
+	 * @param dialog the {@link Dialog} we want to animate
+	 */
 	public DialogOpenAndCloseOptions(Dialog dialog) {
 		this(dialog, Interpolation.linear, Dialog.defaultDuration);
 	}
 	
+	/**
+	 * Construct a new dialog with no {@link Interpolation} and custom duration
+	 * @param dialog the {@link Dialog} we want to animate
+	 * @param duration the duration of the opening and closing animation
+	 */
 	public DialogOpenAndCloseOptions(Dialog dialog, float duration) {
 		this(dialog, Interpolation.linear, duration);
 	}
 	
+	/**
+	 * Construct a new dialog with custom {@link Interpolation} and {@link Dialog#defaultDuration}
+	 * @param dialog the {@link Dialog} we want to animate
+	 * @param interpolation the {@link Interpolation} we want to use for our animation
+	 */
 	public DialogOpenAndCloseOptions(Dialog dialog, Interpolation interpolation) {
 		this(dialog, interpolation, Dialog.defaultDuration);
 	}
 	
+	/**
+	 * Construct a new dialog with custom {@link Interpolation} and duration
+	 * @param dialog the {@link Dialog} we want to animate
+	 * @param interpolation the {@link Interpolation} we want to use for our animation
+	 * @param duration the duration of the opening and closing animation
+	 */
 	public DialogOpenAndCloseOptions(Dialog dialog, Interpolation interpolation, float duration) {
 		this.dialog = dialog;
 		this.frame = dialog.getFrame();
@@ -58,73 +76,133 @@ public class DialogOpenAndCloseOptions {
 		this.duration = duration;
 	}
 	
+	/**
+	 * Helper enum to let us decide what type of animation we want to use,
+	 * currently there are 3 animations.
+	 * @author Johan Fredin
+	 *
+	 */
 	public static enum OpenCloseAnimation {
+		/** Will evenly size the dialog from 0 to its default size */
 		QUAD_EVEN,
+		/** Will first size on the width and then the height */
 		QUAD_WIDTH_FIRST,
+		/** Will fade in / out the dialog */
 		FADE;
 	}
 	
 	/**
-	 * Set the height of the starting line (only useful with quad even animation)
+	 * Set the height of the starting line (only useful with {@link OpenCloseAnimation#QUAD_EVEN} animation)
 	 * @param lineHeight
 	 */
 	public void setLineHeight(float lineHeight) {
 		this.lineHeight = lineHeight;
 	}
 	
+	/**
+	 * Set the duration we want for the opening and closing animation
+	 * @param duration
+	 */
 	public void setDuration(float duration) {
 		this.duration = duration;
 	}
 	
+	/**
+	 * @return the duration for opening and closing the dialog
+	 */
 	public float getDuration() {
 		return duration;
 	}
 	
+	/**
+	 * Lets us set what kind of {@link Interpolation} we want to use for the animation
+	 * @param interpolation
+	 */
 	public void setInterpolation(Interpolation interpolation) {
 		this.interpolation = interpolation;
 	}
 	
+	/**
+	 * Makes the dialog open. Will use the default {@link OpenCloseAnimation#QUAD_EVEN}
+	 * and {@link Dialog#defaultDuration} if no values for these have been previously set.
+	 */
 	public void openDialog() {
 		openDialog(openCloseAnimation, duration);
 	}
 	
+	/**
+	 * Makes the dialog close. Will use the default {@link OpenCloseAnimation#QUAD_EVEN}
+	 * and {@link Dialog#defaultDuration} if no values for these have been previously set.
+	 */
 	public void closeDialog() {
 		closeDialog(openCloseAnimation, duration);
 	}
 	
+	/**
+	 * Makes the dialog open with custom duration.
+	 * will use {@link OpenCloseAnimation#QUAD_EVEN} if none has been previously set
+	 * @param duration the duration of the animation
+	 */
 	public void openDialog(float duration) {
 		openDialog(openCloseAnimation, duration);
 	}
 	
+	/**
+	 * Makes the dialog close with custom duration.
+	 * will use {@link OpenCloseAnimation#QUAD_EVEN} if none has been previously set
+	 * @param duration the duration of the animation
+	 */
 	public void closeDialog(float duration) {
 		closeDialog(openCloseAnimation, duration);
 	}
 	
+	/**
+	 * Makes the dialog open with custom animation. will use {@link Dialog#defaultDuration} if none is previously set.
+	 * @param openCloseAnimation the animation to use
+	 */
 	public void openDialog(OpenCloseAnimation openCloseAnimation) {
 		openDialog(openCloseAnimation, duration);
 	}
 	
+	/**
+	 * Makes the dialog close with custom animation. will use {@link Dialog#defaultDuration} if none is previously set.
+	 * @param openCloseAnimation the animation to use
+	 */
 	public void closeDialog(OpenCloseAnimation openCloseAnimation) {
 		closeDialog(openCloseAnimation, duration);
 	}
 	
-	public void openDialog(OpenCloseAnimation openCloseAnimation, float duration) {
-		float innerDuration = duration / 2;
+	/**
+	 * Makes the dialog open with custom animation and duration
+	 * @param openCloseAnimation the animation to use 
+	 * @param duration the duration of the animation
+	 */
+	public void openDialog(OpenCloseAnimation openCloseAnimation, final float duration) {
+		final float innerDuration = duration / 2;
 		this.dialog.setPosition(x, y);
-		LogUtils.log(new MVPair("Original position is", dialog.getX() + " " + dialog.getY()));
+		float delayTime = duration / 8;
 		
 		switch(openCloseAnimation) {
 		case QUAD_EVEN:
 			this.frame.addAction(Actions.moveBy(-frameWidth / 2, -frameHeight / 2, duration, interpolation));
 			this.dialog.addAction(Actions.moveBy(-width / 2, -height / 2, innerDuration, interpolation));
-			this.frame.addAction(Actions.sequence(Actions.sizeTo(frameWidth, frameHeight, duration, interpolation), Actions.delay(duration / 4), Actions.after(Actions.run(tickRunnable()))));
+			this.frame.addAction(Actions.sequence(Actions.sizeTo(frameWidth, frameHeight, duration, interpolation), Actions.delay(delayTime), Actions.after(Actions.run(tickRunnable()))));
 			this.dialog.addAction(Actions.sizeTo(width, height, innerDuration, interpolation));
 			break;
 		case QUAD_WIDTH_FIRST:
 			this.frame.addAction(Actions.moveBy(-frameWidth / 2, -lineHeight / 2, duration, interpolation));
 			this.dialog.addAction(Actions.moveBy(-width / 2, -lineHeight / 2, innerDuration, interpolation));
 			this.frame.addAction(Actions.sequence(Actions.sizeTo(frameWidth, lineHeight, duration, interpolation)));
-			this.dialog.addAction(Actions.sequence(Actions.sizeTo(width, lineHeight, innerDuration, interpolation), moveAfter(innerDuration, 1, tickRunnable())));
+			this.dialog.addAction(Actions.sequence(Actions.sizeTo(width, lineHeight, innerDuration, interpolation), Actions.delay(delayTime), Actions.after(Actions.run(new Runnable() {
+				@Override
+				public void run() {
+					float halfLine = lineHeight / 2;
+					dialog.addAction(Actions.moveBy(0, (-height / 2 - halfLine), innerDuration, interpolation));
+					frame.addAction(Actions.moveBy(0, (-frameHeight / 2 - halfLine), duration, interpolation));
+					dialog.addAction(Actions.sizeBy(0, (height - lineHeight), innerDuration, interpolation));
+					frame.addAction(Actions.sequence(Actions.sizeBy(0, (frameHeight - lineHeight), duration, interpolation), Actions.after(Actions.run(tickRunnable()))));
+				}
+			}))));
 			break;
 		case FADE:
 			this.dialog.setPosition(dialog.getX() - width / 2, dialog.getY() - height / 2);
@@ -133,20 +211,23 @@ public class DialogOpenAndCloseOptions {
 			this.frame.moveBy(xDiff, yDiff);
 			this.dialog.addAction(Actions.sequence(Actions.fadeOut(0.0f), Actions.after(Actions.run(size(dialog, width, height)))));
 			this.frame.addAction(Actions.sequence(Actions.fadeOut(0.0f), Actions.after(Actions.run(size(frame, frameWidth, frameHeight)))));
-			
 			this.dialog.addAction(Actions.fadeIn(duration, interpolation));
-			this.frame.addAction(Actions.sequence(Actions.fadeIn(duration, interpolation), Actions.after(Actions.run(tickRunnable()))));
+			this.frame.addAction(Actions.sequence(Actions.fadeIn(duration, interpolation), Actions.delay(delayTime), Actions.after(Actions.run(tickRunnable()))));
 
 			break;
 		}
 	}
 	
+	/**
+	 * Makes the dialog close with custom animation and duration
+	 * @param openCloseAnimation the animation to use 
+	 * @param duration the duration of the animation
+	 */
 	public void closeDialog(OpenCloseAnimation openCloseAnimation, final float duration) {
 		this.isTimeToCloseDialog = true;
 		
 		// Empty the text in the box when closing
 		this.dialog.setText("");
-		
 		final float innerDuration = duration / 2;
 				
 		switch(openCloseAnimation) {
@@ -163,7 +244,6 @@ public class DialogOpenAndCloseOptions {
 			this.dialog.addAction(Actions.sequence(Actions.sizeBy(0, -height + (lineHeight), innerDuration, interpolation), Actions.after(Actions.run(new Runnable() {
 				@Override
 				public void run() {
-					
 					dialog.addAction(Actions.moveBy(width / 2, 0, innerDuration, interpolation));
 					frame.addAction(Actions.moveBy(frameWidth / 2, 0, duration, interpolation));
 					dialog.addAction(Actions.sizeBy(-width, 0, innerDuration, interpolation));
@@ -178,10 +258,16 @@ public class DialogOpenAndCloseOptions {
 		}
 	}
 	
+	/**
+	 * @return whether or not the dialog can be closed
+	 */
 	public boolean isTimeToCloseDialog() {
 		return isTimeToCloseDialog;
 	}
 	
+	/**
+	 * @return whether or not we can start displaying text in the dialog
+	 */
 	public boolean isCanTick() {
 		return canTick;
 	}
@@ -195,25 +281,10 @@ public class DialogOpenAndCloseOptions {
 		};
 	}
 	
-	private AfterAction moveAfter(final float innerDuration, final int multiPlier, final Runnable afterRunner) {
-		return Actions.after(Actions.run(new Runnable() {
-			@Override
-			public void run() {
-				float halfLine = lineHeight / 2;
-				
-				dialog.addAction(Actions.moveBy(0, (-height / 2 - halfLine) * multiPlier, innerDuration, interpolation));
-				frame.addAction(Actions.moveBy(0, (-frameHeight / 2 - halfLine) * multiPlier, duration, interpolation));
-				dialog.addAction(Actions.sizeBy(0, (height - lineHeight) * multiPlier, innerDuration, interpolation));
-				frame.addAction(Actions.sequence(Actions.sizeBy(0, (frameHeight - lineHeight) * multiPlier, duration, interpolation), Actions.after(Actions.run(afterRunner))));
-			}
-		}));
-	}
-	
 	private Runnable tickRunnable() {
 		return new Runnable() {
 			@Override
 			public void run() {
-				LogUtils.log(new MVPair("Original position is", dialog.getX() + " " + dialog.getY()));
 				canTick = true;
 			}
 		};
