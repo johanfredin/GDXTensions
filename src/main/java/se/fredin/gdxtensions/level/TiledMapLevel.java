0f1649e6c55f53312229faa7e52068f2dfd83368
Johan Fredin <johanfredin204@hotmail.com>
2015-02-19 20:45:36 +0100

    Made a tiled map utility class and added documentation, also cleaned up a bit

--- src/main/java/se/fredin/gdxtensions/level/TiledMapLevel.java
+++ src/main/java/se/fredin/gdxtensions/level/TiledMapLevel.java
@@ -3,20 +3,17 @@ package se.fredin.gdxtensions.level;
 import se.fredin.gdxtensions.assetmanagement.Assets;
 import se.fredin.gdxtensions.collision.CollisionHandler;
 import se.fredin.gdxtensions.screen.BaseScreen;
+import se.fredin.gdxtensions.utils.TiledMapUtils;
 
-import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.utils.Array;
 
 /***
- * Extension of Level, used when we want to create a level that works with a tiled map
- * created in Tiled or any other editor supporting the .TMX format. contains a TiledMap and a
- * OrthogonalTiledMapRenderer for further functionality. Also comes equipped with a bunch of neat little 
+ * Used when we want to create a {@link Level} that works with a tiled map
+ * created in Tiled or any other editor supporting the .TMX format. Contains a {@link TiledMap}, 
+ * {@link OrthogonalTiledMapRenderer} and a {@link CollisionHandler}. Also comes equipped with
+ * an instance of {@link TiledMapUtils} that comes with a bunch of 
  * helper methods for quickly retrieving map objects, properties and layers.
  * @author Johan Fredin
  *
@@ -27,21 +24,28 @@ public abstract class TiledMapLevel<T extends BaseScreen> extends Level {
 	
 	protected OrthogonalTiledMapRenderer mapRenderer;
 	protected TiledMap map;
+	protected TiledMapUtils tiledMapUtils;
 	protected CollisionHandler collisionHandler;
 	protected int mapWidth;
 	protected int mapHeight;
 	protected int tileWidth;
 	protected int tileHeight;
 	
+	/**
+	 * Construct a new {@link TiledMapLevel}
+	 * @param levelName the path and name of the .tmx map
+	 * @param screen the {@link Screen} that will render this level
+	 */
 	@SuppressWarnings("unchecked")
 	public TiledMapLevel(String levelName, T screen) {
 		super(screen);
 		this.map = (TiledMap) Assets.getInstance().get(levelName);
-		this.tileWidth = map.getProperties().get("tilewidth", Integer.class);
-		this.tileHeight = map.getProperties().get("tileheight", Integer.class);
-		this.mapWidth = map.getProperties().get("width", Integer.class) * tileWidth;
-		this.mapHeight = map.getProperties().get("height", Integer.class) * tileHeight;
-		this.mapRenderer = new OrthogonalTiledMapRenderer(map, screen.getSpriteBatch());
+		this.tiledMapUtils = new TiledMapUtils(this.map);
+		this.tileWidth = this.tiledMapUtils.getTileWidth();
+		this.tileHeight = this.tiledMapUtils.getTileHeight();
+		this.mapWidth = this.tiledMapUtils.getMapWidth();
+		this.mapHeight = this.tiledMapUtils.getMapHeight();
+		this.mapRenderer = new OrthogonalTiledMapRenderer(this.map, screen.getSpriteBatch());
 	}
 	
 	/**
@@ -65,7 +69,7 @@ public abstract class TiledMapLevel<T extends BaseScreen> extends Level {
 	public TiledMap getMap() {
 		return map;
 	}
-
+	
 	/**
 	 * Set what {@link TiledMap} to use
 	 * @param map the map to use
@@ -75,122 +79,10 @@ public abstract class TiledMapLevel<T extends BaseScreen> extends Level {
 	}
 	
 	/**
-	 * @return the width of the map e.g the width * tile width of each tile
+	 * @return the {@link TiledMapUtils} instance
 	 */
-	public int getMapWidth() {
-		return mapWidth;
-	}
-	
-	/**
-	 * @return the height of the map e.g the height * height of each tile
-	 */
-	public int getMapHeight() {
-		return mapHeight;
-	}
-	
-	/**
-	 * @return the width of the tiles
-	 */
-	public int getTileWidth() {
-		return tileWidth;
-	}
-	
-	/**
-	 * @return the height of the tiles
-	 */
-	public int getTileHeight() {
-		return tileHeight;
-	}
-	
-	/**
-	 * Lets us retrieve a raw object type by class, users calling this method will need to cast
-	 * @param objectLayerName
-	 * @param type
-	 * @return
-	 */
-	@SuppressWarnings("unchecked")
-	public Object getMapObjectLayerByType(String objectLayerName, Class type) {
-		return map.getLayers().get(objectLayerName).getObjects().getByType(type);
-	}
-	
-	/**
-	 * Let's us get a finished {@link Rectangle} array from given {@link RectangleMapObject}s
-	 * Very useful for getting rectangles for collision etc... right away. 
-	 * @param objectLayerName the name of the object layer
-	 * @return a new {@link Array} containing the map object layer
-	 */
-	public Array<Rectangle> getRectangularMapObjects(String objectLayerName) {
-		Array<RectangleMapObject> rectangleMapObjects = map.getLayers().get(objectLayerName).getObjects().getByType(RectangleMapObject.class);
-		Array<Rectangle> rectangles = new Array<Rectangle>();
-		for(RectangleMapObject rectangleMapObject : rectangleMapObjects) {
-			rectangles.add(rectangleMapObject.getRectangle());
-		}
-		return rectangles;
-	}
-	
-	/**
-	 * Lets us retrieve a map object.
-	 * @param layerName the object layer we want to get the map object from
-	 * @param objectName the name of the map object
-	 * @return the map object with given name from object layer
-	 */
-	public MapObject getMapObject(String layerName, String objectName) {
-		return map.getLayers().get(layerName).getObjects().get(objectName);
-	}
-	
-	/**
-	 * Lets us retrieve a {@link TiledMapTileLayer} from the map.
-	 * @param layerName the name of the layer
-	 * @return the layer
-	 */
-	public TiledMapTileLayer getLayer(String layerName) {
-		return (TiledMapTileLayer) map.getLayers().get(layerName);
-	}
-	
-	/**
-	 * @param mapObject the {@link MapObject} we need the property from
-	 * @param propertyName the name of the property
-	 * @return the Float property corresponding to given map object and property name
-	 */
-	public float getFloatProperty(MapObject mapObject, String propertyName) {
-		return mapObject.getProperties().get(propertyName, Float.class);
-	}
-	
-	/**
-	 * @param mapObject the {@link MapObject} we need the property from
-	 * @param propertyName the name of the property
-	 * @return the Boolean property corresponding to given map object and property name
-	 */
-	public boolean getBooleanProperty(MapObject mapObject, String propertyName) {
-		return mapObject.getProperties().get(propertyName, Boolean.class);
-	}
-	
-	/**
-	 * @param mapObject the {@link MapObject} we need the property from
-	 * @param propertyName the name of the property
-	 * @return the Integer property corresponding to given map object and property name
-	 */
-	public int getIntegerProperty(MapObject mapObject, String propertyName) {
-		return mapObject.getProperties().get(propertyName, Integer.class);
-	}
-
-	/**
-	 * @param mapObject the {@link MapObject} we need the property from
-	 * @param propertyName the name of the property
-	 * @return the String property corresponding to given map object and property name
-	 */
-	public String getStringProperty(MapObject mapObject, String propertyName) {
-		return mapObject.getProperties().get(propertyName, String.class);
-	}
-	
-	/**
-	 * @param mapObject the {@link MapObject} we need the positions from
-	 * @param xPosition the property we want as x position
-	 * @param yPosition the property we want as y position
-	 * @return 
-	 */
-	public Vector2 getVector2(MapObject mapObject, String xPosition, String yPosition) {
-		return new Vector2(getFloatProperty(mapObject, xPosition), getFloatProperty(mapObject, yPosition));
+	public TiledMapUtils getTiledMapUtils() {
+		return tiledMapUtils;
 	}
 	
 	@Override
---
+++ src/main/java/se/fredin/gdxtensions/utils/TiledMapUtils.java
@@ -0,0 +1,215 @@
+package se.fredin.gdxtensions.utils;
+
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+
+/**
+ * The {@link TiledMapUtils} class comes equipped with a whole bunch of helpful
+ * methods for retrieving the most common as well as custom properties, layers, objects
+ * and positions. 
+ * @author Johan Fredin
+ *
+ */
+public class TiledMapUtils {
+	
+	private TiledMap map;
+	
+	/**
+	 * Construct a new {@link TiledMapUtils} instance
+	 * users will need to set which {@link TiledMap} to use 
+	 * manually otherwise you will get {@link NullPointerException}s when
+	 * trying to call any of the methods
+	 */
+	public TiledMapUtils(){}
+	
+	/**
+	 * Construct a new {@link TiledMapUtils} instance.
+	 * @param map the {@link TiledMap} to operate on
+	 */
+	public TiledMapUtils(TiledMap map) {
+		this.map = map;
+	}
+	
+	/**
+	 * @return the tiled map used
+	 */
+	public TiledMap getMap() {
+		return map;
+	}
+
+	/**
+	 * Set what {@link TiledMap} to use
+	 * @param map the map to use
+	 */
+	public void setMap(TiledMap map) {
+		this.map = map;
+	}
+	
+	/**
+	 * @return the width of the map e.g the width * tile width of each tile
+	 */
+	public int getMapWidth() {
+		return getIntegerMapProperty("width") * getIntegerMapProperty("tilewidth");
+	}
+	
+	/**
+	 * @return the height of the map e.g the height * tile height of each tile
+	 */
+	public int getMapHeight() {
+		return getIntegerMapProperty("height") * getIntegerMapProperty("tileheight");
+	}
+	
+	/**
+	 * @return the width of the tiles
+	 */
+	public int getTileWidth() {
+		return getIntegerMapProperty("tilewidth");
+	}
+	
+	/**
+	 * @return the height of the tiles
+	 */
+	public int getTileHeight() {
+		return getIntegerMapProperty("tileheight");
+	}
+	
+	/**
+	 * Lets us retrieve a map object layer, could be rectangles used for detecting collision
+	 * or other custom triggers. Returns an {@link Object} so users calling this method will
+	 * need to cast it to the appropriate type.
+	 * @param objectLayerName the name of the object layer
+	 * @return the {@link Object} associated with the object layer name passed in
+	 */
+	public Object getMapObjectLayerByType(String objectLayerName) {
+		return map.getLayers().get(objectLayerName).getObjects();
+	}
+	
+	/**
+	 * Lets us retrieve a map property like tile width, map width or custom properties
+	 * like a time limit for example. This method return an {@link Object} so users calling
+	 * this method will need to cast it to appropriate type.
+	 * @param propertyName the name of the map property.
+	 * @return the {@link Object} associated with the propertyName passed in
+	 */
+	public Object getMapProperty(String propertyName) {
+		return map.getProperties().get(propertyName);
+	}
+	
+	/**
+	 * Let's us get a finished {@link Rectangle} array from given {@link RectangleMapObject}s
+	 * Very useful for getting rectangles for collision etc... right away. 
+	 * @param objectLayerName the name of the object layer
+	 * @return a new {@link Array} containing the map object layer
+	 */
+	public Array<Rectangle> getRectangularMapObjects(String objectLayerName) {
+		Array<RectangleMapObject> rectangleMapObjects = map.getLayers().get(objectLayerName).getObjects().getByType(RectangleMapObject.class);
+		Array<Rectangle> rectangles = new Array<Rectangle>();
+		for(RectangleMapObject rectangleMapObject : rectangleMapObjects) {
+			rectangles.add(rectangleMapObject.getRectangle());
+		}
+		return rectangles;
+	}
+	
+	/**
+	 * Lets us retrieve a map object.
+	 * @param objectLayerName the object layer we want to get the map object from
+	 * @param objectName the name of the map object
+	 * @return the map object with given name from object layer
+	 */
+	public MapObject getMapObject(String objectLayerName, String objectName) {
+		return map.getLayers().get(objectLayerName).getObjects().get(objectName);
+	}
+	
+	/**
+	 * Lets us retrieve a {@link TiledMapTileLayer} from the map.
+	 * @param layerName the name of the layer
+	 * @return the layer
+	 */
+	public TiledMapTileLayer getLayer(String layerName) {
+		return (TiledMapTileLayer) map.getLayers().get(layerName);
+	}
+	
+	/**
+	 * @param mapObject the {@link MapObject} we need the property from
+	 * @param propertyName the name of the property
+	 * @return the Float property corresponding to given map object and property name
+	 */
+	public float getFloatMapObjectProperty(MapObject mapObject, String propertyName) {
+		return mapObject.getProperties().get(propertyName, Float.class);
+	}
+	
+	/**
+	 * @param mapObject the {@link MapObject} we need the property from
+	 * @param propertyName the name of the property
+	 * @return the Boolean property corresponding to given map object and property name
+	 */
+	public boolean getBooleanMapObjectProperty(MapObject mapObject, String propertyName) {
+		return mapObject.getProperties().get(propertyName, Boolean.class);
+	}
+	
+	/**
+	 * @param mapObject the {@link MapObject} we need the property from
+	 * @param propertyName the name of the property
+	 * @return the Integer property corresponding to given map object and property name
+	 */
+	public int getIntegerMapObjectProperty(MapObject mapObject, String propertyName) {
+		return mapObject.getProperties().get(propertyName, Integer.class);
+	}
+
+	/**
+	 * @param mapObject the {@link MapObject} we need the property from
+	 * @param propertyName the name of the property
+	 * @return the String property corresponding to given map object and property name
+	 */
+	public String getStringMapObjectProperty(MapObject mapObject, String propertyName) {
+		return mapObject.getProperties().get(propertyName, String.class);
+	}
+	
+	/**
+	 * @param propertyName the name of the property e.g the key
+	 * @return the {@link String} property corresponding to the key passed in
+	 */
+	public String getStringMapProperty(String propertyName) {
+		return map.getProperties().get(propertyName, String.class);
+	}
+	
+	/**
+	 * @param propertyName the name of the property e.g the key
+	 * @return the {@link Float} property corresponding to the key passed in
+	 */
+	public float getFloatMapProperty(String propertyName) {
+		return map.getProperties().get(propertyName, Float.class);
+	}
+	
+	/**
+	 * @param propertyName the name of the property e.g the key
+	 * @return the {@link Integer} property corresponding to the key passed in
+	 */
+	public int getIntegerMapProperty(String propertyName) {
+		return map.getProperties().get(propertyName, Integer.class);
+	}
+	
+	/**
+	 * @param propertyName the name of the property e.g the key
+	 * @return the {@link Boolean} property corresponding to the key passed in
+	 */
+	public boolean getBooleanMapProperty(String propertyName) {
+		return map.getProperties().get(propertyName, Boolean.class);
+	}
+	
+	/**
+	 * @param mapObject the {@link MapObject} we need the positions from
+	 * @param xPosition the property we want as x position
+	 * @param yPosition the property we want as y position
+	 * @return 
+	 */
+	public Vector2 getVector2(MapObject mapObject, String xPosition, String yPosition) {
+		return new Vector2(getFloatMapObjectProperty(mapObject, xPosition), getFloatMapObjectProperty(mapObject, yPosition));
+	}
+
+}
